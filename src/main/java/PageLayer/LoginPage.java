f7378ef4fbc460375d6710c96f9413b47e48b70f
LENOVO <LENOVO@DESKTOP-DK6U8K1.bbrouter>
2024-01-14 01:10:44 +0530

    Sushant the Lead Trainer

--- src/main/java/PageLayer/LoginPage.java
+++ src/main/java/PageLayer/LoginPage.java
@@ -24,6 +24,7 @@ public class LoginPage extends BaseClass {
 	
 	public void loginPage(String uname, String pwd)
 	{
+		System.out.println("Sushant the Lead Trainer");
 		System.out.println("Dushyant The Lead");
 		UtlityMethods.sendKeys(username, uname);
 		UtlityMethods.sendKeys(password, pwd);
